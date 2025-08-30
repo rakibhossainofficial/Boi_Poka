7e01da0b3fcb79a5b833cd03a8ca2cf702a6a234
rakibhossainofficial <rkrrakib20@gmail.com>
2025-08-31 00:16:46 +0600

    Show All button Added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
 import BooksSection from "../../Components/BooksSection/BooksSection.jsx";
 import Hero from "../../Components/Hero/Hero.jsx";
 import { useLoaderData } from "react-router";
-import { Suspense } from "react";
-
+import { Suspense, useEffect, useState } from "react";
 
 const Home = () => {
   const booksData = useLoaderData();
+
+  const [displayBooks, setDisplayBooks] = useState([]);
+  const [showAll, setShowAll] = useState(false);
+
+  useEffect(() => {
+    if (showAll) {
+      setDisplayBooks(booksData);
+    } else setDisplayBooks(booksData.slice(0, 4));
+  }, [booksData, showAll]);
 
   return (
     <div>
@@ -15,7 +23,7 @@ const Home = () => {
 
       <Suspense fallback={<span>Loading...</span>}>
         <div className=" grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
-          {booksData.map((singlebooks) => (
+          {displayBooks.map((singlebooks) => (
             <BooksSection
               key={singlebooks.bookId}
               singlebooks={singlebooks}
@@ -23,9 +31,17 @@ const Home = () => {
           ))}
         </div>
       </Suspense>
+
+      <div className="flex items-center justify-center">
+        <button
+          onClick={() => setShowAll(!showAll)}
+          className="px-4 py-2 border my-5 rounded-lg cursor-pointer"
+        >
+          {showAll ? "Show Less" : "Show All"}
+        </button>
+      </div>
     </div>
   );
 };
 
 export default Home;
-
